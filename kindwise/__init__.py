0a2317e1b8d7fe6ceccd44b275392e1a0eba2fb7
Simon Plhak <simon.plhak@flowerchecker.com>
2024-04-10 16:52:37 +0200

    fix: optimize imports in __init__ and add ClassificationLevel model

--- kindwise/__init__.py
+++ kindwise/__init__.py
@@ -1,6 +1,6 @@
-from kindwise.models import Result, Identification, Input, UsageInfo
-from kindwise.plant import PlantIdentification, HealthAssessment, RawPlantIdentification
-from kindwise.plant import PlantApi
-from kindwise.insect import InsectApi
-from kindwise.mushroom import MushroomApi
 from kindwise.crop_health import CropHealthApi
+from kindwise.insect import InsectApi
+from kindwise.models import Result, Identification, Input, UsageInfo, ClassificationLevel
+from kindwise.mushroom import MushroomApi
+from kindwise.plant import PlantApi
+from kindwise.plant import PlantIdentification, HealthAssessment, RawPlantIdentification
