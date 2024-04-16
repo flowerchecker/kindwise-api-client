ea000c0b57edd85cfa425bb44718e3bb4f8d2432
Jiří Thran Řihák <exthran@gmail.com>
2024-04-16 17:37:43 +0200

    feat: add client for snake.id

---
+++ kindwise/snake.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from kindwise import settings
+from kindwise.core import KindwiseApi
+from kindwise.models import Identification
+
+
+class SnakeApi(KindwiseApi[Identification]):
+    host = 'https://snake.kindwise.com'
+
+    def __init__(self, api_key: str = None):
+        api_key = settings.INSECT_API_KEY if api_key is None else api_key
+        if api_key is None:
+            raise ValueError(
+                'API key is required, set it in init method of class or in .env file under "SNAKE_API_KEY" key'
+            )
+        super().__init__(api_key)
+
+    @property
+    def identification_url(self):
+        return f'{self.host}/api/v1/identification'
+
+    @property
+    def usage_info_url(self):
+        return f'{self.host}/api/v1/usage_info'
+
+    @property
+    def views_path(self) -> Path:
+        return settings.APP_DIR / 'resources' / f'views.snake.json'
