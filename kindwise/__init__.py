8687791f66b725be9fcd2332357fb734c1b35109
Simon Plhak <simon.plhak@flowerchecker.com>
2024-05-30 12:11:16 +0200

    fix: include conversation models into root __init__ import directly from kindwise

--- kindwise/__init__.py
+++ kindwise/__init__.py
@@ -1,5 +1,14 @@
 from kindwise.crop_health import CropHealthApi
 from kindwise.insect import InsectApi, InsectKBType
-from kindwise.models import Result, Identification, Input, UsageInfo, ClassificationLevel, SearchResult
+from kindwise.models import (
+    Result,
+    Identification,
+    Input,
+    UsageInfo,
+    ClassificationLevel,
+    SearchResult,
+    MessageType,
+    Conversation,
+)
 from kindwise.mushroom import MushroomApi, MushroomKBType
 from kindwise.plant import PlantApi, PlantIdentification, HealthAssessment, RawPlantIdentification, PlantKBType
