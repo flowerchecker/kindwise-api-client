1dbc0809571fb71b9f3fbdd10a53d7c910c89840
Simon Plhak <simon.plhak@flowerchecker.com>
2026-01-12 11:59:07 +0100

    fix: await test method in test_crop in async_api tests

--- kindwise/tests/async_api/test_crop.py
+++ kindwise/tests/async_api/test_crop.py
@@ -5,8 +5,8 @@ from kindwise.tests.conftest import IMAGE_DIR, run_async_test_requests_to_server
 
 
 @pytest.mark.asyncio
-def test_requests_to_crop_server(api_key):
-    run_async_test_requests_to_server(
+async def test_requests_to_crop_server(api_key):
+    await run_async_test_requests_to_server(
         AsyncCropHealthApi(api_key=api_key),
         'crop',
         IMAGE_DIR / 'potato.late_blight.jpg',
