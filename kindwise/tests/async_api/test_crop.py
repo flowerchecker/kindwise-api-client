1f19256a5e860cfac44921327e2ae957ba656d83
Simon Plhak <simon.plhak@flowerchecker.com>
2026-01-12 11:59:51 +0100

    fix: do not duplicate test for availabel details, keep this test only in sync test version

--- kindwise/tests/async_api/test_crop.py
+++ kindwise/tests/async_api/test_crop.py
@@ -1,7 +1,7 @@
 import pytest
 
 from kindwise.async_api.crop_health import AsyncCropHealthApi, CropIdentification
-from kindwise.tests.conftest import IMAGE_DIR, run_async_test_requests_to_server, run_test_available_details
+from kindwise.tests.conftest import IMAGE_DIR, run_async_test_requests_to_server
 
 
 @pytest.mark.asyncio
@@ -12,33 +12,4 @@ async def test_requests_to_crop_server(api_key):
         IMAGE_DIR / 'potato.late_blight.jpg',
         CropIdentification,
         model_name='disease',
-    )
-
-
-def test_available_details(api_key):
-    expected_view_names = {
-        'common_names',
-        'description',
-        'eppo_code',
-        'eppo_regulation_status',
-        'gbif_id',
-        'image',
-        'images',
-        'severity',
-        'spreading',
-        'symptoms',
-        'taxonomy',
-        'treatment',
-        'type',
-        'wiki_description',
-        'wiki_url',
-    }
-    expected_license = {'wiki_description', 'image', 'images'}
-    expected_localized = {'common_names', 'wiki_url', 'wiki_description'}
-
-    run_test_available_details(
-        expected_view_names,
-        expected_license,
-        expected_localized,
-        AsyncCropHealthApi(api_key=api_key).available_details(),
     )
--- kindwise/tests/async_api/test_insect.py
+++ kindwise/tests/async_api/test_insect.py
@@ -1,36 +1,11 @@
 import pytest
 from kindwise.async_api.insect import AsyncInsectApi
 from kindwise.models import Identification
-from kindwise.tests.conftest import IMAGE_DIR, run_async_test_requests_to_server, run_test_available_details
+from kindwise.tests.conftest import IMAGE_DIR, run_async_test_requests_to_server
 
 
 @pytest.mark.asyncio
 async def test_requests_to_insect_server(api_key):
     await run_async_test_requests_to_server(
         AsyncInsectApi(api_key=api_key), 'insect', IMAGE_DIR / 'bee.jpeg', Identification
-    )
-
-
-def test_available_details(api_key):
-    expected_view_names = {
-        'common_names',
-        'danger',
-        'danger_description',
-        'description',
-        'gbif_id',
-        'image',
-        'images',
-        'inaturalist_id',
-        'rank',
-        'red_list',
-        'role',
-        'synonyms',
-        'taxonomy',
-        'url',
-    }
-    expected_license = {'description', 'image', 'images'}
-    expected_localized = {'common_names', 'url', 'description'}
-
-    run_test_available_details(
-        expected_view_names, expected_license, expected_localized, AsyncInsectApi(api_key=api_key).available_details()
     )
